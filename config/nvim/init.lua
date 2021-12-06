72dd7f64cd8ea8570b883f1a93564512d7f0c761
Htoo Pyae <htoopyae.dev@gmail.com>
2021-12-06 22:26:33 +0630

    feat: Added lualine, bufferline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,8 @@
 require('plugins')
 require('options')
 require('treesitter-config')
+require('lualine-config')
+require('bufferline-config')
 
 -- Setting the Colorscheme
 vim.cmd('colorscheme onedark')
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -7,5 +7,14 @@ return require('packer').startup(function()
 
   -- Tree-sitter
   use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+
+  -- Status line
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+
+  -- Buffer line
+  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
 end)
 
