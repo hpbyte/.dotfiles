0d7892ce332a5a5e1f55bb2c5d9e7472fd84733b
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-17 22:41:42 +0700

    chore: remove nvim

--- config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require('plugins')
-require('options')
-require('treesitter-config')
-require('lualine-config')
-require('bufferline-config')
-require('nvim-tree-config')
-
--- Setting the Colorscheme
-vim.cmd('colorscheme onedark')
--- config/nvim/lua/treesitter-config/init.lua
+++
@@ -1,7 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"html", "javascript", "typescript", "lua"}, 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
